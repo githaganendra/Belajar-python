87e904909cfb6b9a04d225c668a945c084828b83
githaganendra <ganendragitha@gmail.com>
2024-03-30 16:44:42 +0700

    push

---
+++ latihan_python/latihan4.py
@@ -0,0 +1,25 @@
+var1 = "Hello World"
+var2 = "I Love Python"
+print("var1[0]", var1[0])
+print("var2[2:6]", var2[2:6])
+
+var3 = "Hello World"
+var4 = var3[:11]
+print("Data update: ", var4 + "Python")
+
+str1 = "Hello"
+str2 = "Python"
+print(str1 + " " + str2)
+str3 = str1 + " " + str2
+print(str3)
+
+print('''Kemarin hari Jum'at''')
+print('Kemarin hari Jum\'at')
+print("Kemarin hari Jum'at")
+
+str4 = len('Kemarin hari Jum\'at')
+str5 = len("Kemarin hari Jum'at")
+print(str4)
+print(str5)
+
+print("Ini adalah baris pertama \nDan ini adalah baris kedua")
---
+++ latihan_python/tugas2daspro.lat1.py
@@ -0,0 +1,5 @@
+#Latihan 1
+
+#Contoh Operasi Penugasan
+nilai1 = int(input("Masukkan Nilai Pertama : 2"))
+nilai2 = int(input("Masukkan Nilai Kedua : 5"))
